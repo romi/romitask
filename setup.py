145022497896daada44b521f55eaf9af08f1199a
Nabil AIT TALEB <aittalebnabil@gmail.com>
2022-07-05 15:06:24 +0200

    back to setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, Extension, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+s = setup(
+    name="romitask",
+    version="0.10.99",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    scripts=[
+        'bin/romi_run_task',
+        'bin/print_task_info'
+    ],
+    author="Nabil Ait Taleb",
+    author_email="mohamednabil.aittaleb@sony.com",
+    description="A romi task runner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'luigi',
+        'toml',
+        'watchdog',
+        'colorlog'
+    ],
+    include_package_data=True
+)
