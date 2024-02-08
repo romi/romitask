5680fc1f7e5daeda0d3ac8f5d279dda935c0d226
Jonathan LEGRAND <jonathan.legrand@ens-lyon.fr>
2024-02-08 14:48:51 +0100

    Update `setup.py`.

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from setuptools import find_packages
 from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-s = setup(
+opts = dict(
     name="romitask",
-    version="0.10.99",  # overriden by `use_scm_version=True`
+    version="0.10.99",
+    description="The ROMI task runner, a luigi based task pipeline.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nabil Ait Taleb",
+    author_email="mohamednabil.aittaleb@sony.com",
+    maintainer='Jonathan Legrand',
+    maintainer_email='jonathan.legrand@ens-lyon.fr',
+    url="https://docs.romi-project.eu/Scanner/home/",
+    download_url='',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     scripts=[
         'bin/romi_run_task',
         'bin/print_task_info'
     ],
-    author="Nabil Ait Taleb",
-    author_email="mohamednabil.aittaleb@sony.com",
-    description="The ROMI task runner, a luigi based task pipeline.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     zip_safe=False,
     python_requires='>=3.7',
-    # use_scm_version=True,
-    # setup_requires=['setuptools_scm'],
     install_requires=[],
     include_package_data=True
 )
+
+if __name__ == '__main__':
+    setup(**opts)
